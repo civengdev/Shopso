51ec0fe224ce38eab1089376eac3962278b8aeaf
Miguel Andrews <scirea.andrews@gmail.com>
2015-10-17 13:04:46 -0500

    non-interactive

---
+++ js/app.js
@@ -0,0 +1,49 @@
+
+/* 1) on page load (inside document ready function)*/
+$(document).ready(function() {
+	/*
+	a) add list title
+	--> onclick get the value from the "List Title" input and display it as List Title
+	*/
+	function addListTitle () {
+
+	}
+	/*
+	b) delete list
+	--> onclick delete the title in the List Title
+	*/
+	function deleteListTitle () {
+
+	}
+	/*
+	c) add list item
+	--> onclick get the value from the "Add item here" input and display it inside the shopping list item list bellow (by adding one row at at a time)
+	*/
+	function addListItem () {
+
+	}
+	console.log( "ready!" );
+});
+/*
+2) after the page load (out side document ready function)
+a) select an item in the list
+--> onclick toggle the "select" class (defined as a stand alone class in css) on the item
+*/
+
+function selectListItem () {
+
+}
+/*
+b) delete an item in the list
+--> onclick delete the entire row (it's parent container) in which the button exists
+*/
+function deleteListItem () {
+
+}
+/*
+c) clear the entire list
+--> on click delete all the rows in the shopping list
+*/
+function clearEntireList () {
+
+}
